3c51c6b64e1e8e9ab9192e641c3d908b1291b9e4
Francesco <fra.dimiccoli@gmail.com>
2016-02-26 18:24:54 +0100

    git final update

--- Lancia/Lancia.pl
+++ Lancia/Lancia.pl
@@ -3,8 +3,8 @@
 use warnings;
 chomp (@LanciaDati = `ls | grep Dati`);
 $num_Dati = scalar(@LanciaDati);
-system("rm /home/AMS/fdimicco/fdimicco/fdimicco/Deutons/logs/*");
-system("rm /home/AMS/fdimicco/fdimicco/fdimicco/Deutons/err/*");
+system("rm /home/AMS/fdimicco/fdimicco/fdimicco/Deutons/logs/*.log");
+system("rm /home/AMS/fdimicco/fdimicco/fdimicco/Deutons/err/*.err");
 if($ARGV[0]!=0&&$ARGV[2]!=1) {
 system("rm /home/AMS/fdimicco/fdimicco/fdimicco/MAIN/sommadati/sommadati*");
 system("perl /home/AMS/fdimicco/fdimicco/fdimicco/MAIN/perl/Sommaisto2.pl $ARGV[0] $ARGV[1]");
