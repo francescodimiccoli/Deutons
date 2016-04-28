5f90afe5b2efcd32e1e005b14d0f82c9438abbfb
Francesco <fra.dimiccoli@gmail.com>
2016-04-28 12:22:26 +0200

    general cuts

--- CodesforAnalysis/Parte2/Cuts.cpp
+++ CodesforAnalysis/Parte2/Cuts.cpp
@@ -2,18 +2,12 @@ void Cuts(){
 	//likelihood cut
 	Likcut=false;
 	if((((((int)Cutmask)>>11)==512||(((int)Cutmask)>>11)==0)&&-log(1-LDiscriminant)>2.6)||((!((((int)Cutmask)>>11)==512||(((int)Cutmask)>>11)==0))&&-log(1-LDiscriminant)>0.55)) Likcut=true;
-	//for some reason, Distance cut doesn't reject pions, adding a low mass cut
-	//mass cut
-	float mass=0;
-	float massRICH=0;
-	bool masscut=false;
-	if(BetaRICH>0) massRICH=(R/BetaRICH)*pow(1-pow(BetaRICH,2),0.5); else massRICH=999;
-	mass=(R/Beta)*pow(1-pow(Beta,2),0.5); 
-	if(mass>0.5&&massRICH>0.5) masscut=true;
+	
 	//distance cut
 	Distcut=false;
 	//if(fabs((Dist5D_P-Dist5D)/(Dist5D_P+Dist5D))>0.2&&masscut) Distcut=true;
 	if((Dist5D<4||Dist5D_P<4)) Distcut=true;
+	
 	//Helium rej. cut
 	float EdepTOFud=(EdepTOFU+EdepTOFD)/2;
 	Herejcut=false;
