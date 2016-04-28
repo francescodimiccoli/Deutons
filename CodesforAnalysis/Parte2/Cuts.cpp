c78dacfe92f3428a792f245bcc03759a637fcdf9
Francesco <fra.dimiccoli@gmail.com>
2016-04-28 12:38:38 +0200

    adding strong beta cut to general cuts

--- CodesforAnalysis/Parte2/Cuts.cpp
+++ CodesforAnalysis/Parte2/Cuts.cpp
@@ -1,18 +1,31 @@
 void Cuts(){
+	/////////////////////Analysis cuts//////////////////////////
+	
 	//likelihood cut
 	Likcut=false;
 	if((((((int)Cutmask)>>11)==512||(((int)Cutmask)>>11)==0)&&-log(1-LDiscriminant)>2.6)||((!((((int)Cutmask)>>11)==512||(((int)Cutmask)>>11)==0))&&-log(1-LDiscriminant)>0.55)) Likcut=true;
 	
-	//distance cut
+	//Distance cut
 	Distcut=false;
-	//if(fabs((Dist5D_P-Dist5D)/(Dist5D_P+Dist5D))>0.2&&masscut) Distcut=true;
 	if((Dist5D<4||Dist5D_P<4)) Distcut=true;
+	
+	////////////////////////////////////////////////////////////
+	
+	
+	/////////////////////Control sample cuts//////////////////////
 	
 	//Helium rej. cut
 	float EdepTOFud=(EdepTOFU+EdepTOFD)/2;
 	Herejcut=false;
 	if(fabs(EdepTrackbeta->Eval(Beta)-EdepTrack)/(pow(EdepTrackbeta->Eval(Beta),2)*etrack->Eval(Beta))<4||fabs(EdepTOFbeta->Eval(Beta)-EdepTOFud)/(pow(EdepTOFbeta->Eval(Beta),2)*etofu->Eval(Beta))<10)
 	Herejcut=true;	
+
+	//Strong beta cut
+	if(	(((((int)Cutmask)>>11)==512)&&BetaRICH<0.97 )||
+		(((((int)Cutmask)>>11)== 0 )&&BetaRICH<0.985)|| 	
+		Beta < 0.8
+	  ) 	
+	Betastrongcut = true;
 }
 
 
