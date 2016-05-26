4afee50a336ba00486ef9d2815688cb965d3ffec
Laurent Basara <lbasara@lxplus.cern.ch>
2016-05-26 13:05:37 +0200

    Finished cleaning FluxClass.cpp ; now the hard part begins

--- CodesforAnalysis/Parte2/FluxClass.cpp
+++ CodesforAnalysis/Parte2/FluxClass.cpp
@@ -1,15 +1,7 @@
 using namespace std;
 
 
-void TimeGeo (TH2 * Exposure, TH1 * Tempi)
-{
-   for (int R=0; R<Exposure ->GetNbinsX() ; R++ )
-      for (int lat =1; lat< Tempi->GetNbinsX(); lat++) {
-         Exposure  -> SetBinContent (R+1,lat+1,Tempi -> GetBinContent (lat) );
-         Exposure  -> SetBinError (R+1,lat+1,10);
-      }
-   return;
-}
+
 
 
 
@@ -17,7 +9,6 @@ class Flux {
    private:
       //Delta E;
       TH1 * DeltaE;
-
       std::string suffixname;
       Binning bins;
 
@@ -30,43 +21,18 @@ class Flux {
 
       std::string name;
 
-
       ///creation constructor
       Flux (std::string basename, std::string suffix, Binning B)
-      {
-         string hname=basename + "Counts_" + suffix;
-         Counts	 = new TH1F ( hname.c_str(), hname.c_str(),B.size(),0,B.size());
-         name = basename;
-         suffixname = suffix;
-         bins=Bin;
-      }
-
       Flux (std::string basename, std::string suffix, Binning B, int n)
-      {
-         string hname=basename + "Counts_" + suffix;
-         Counts   = new TH2F ( hname.c_str(), hname.c_str(), B.size(),  0, B.size(),  n,0,n);
-         name = basename;
-         suffixname = suffix;
-         bins=Bin;
-      }
-
-      //reading constructor
+      ///reading constructor
       Flux (TFile * file, std::string basename, std::string suffix, std::string dirname, std::string acceptname, Binning B)
-      {
-         Counts	 = (TH1 *) file->Get ( (basename + "Counts_" + suffix       ).c_str() );
-         Acceptance  = (TH1 *) file->Get ( ("/" + dirname +"/" +acceptname + "_" + suffix  ).c_str() );
-         name = basename;
-         suffixname = suffix;
-         bins=Bin;
-      }
-
+      
       void Write();
-
-      void Set_Exposure_Time (TH2 * ExposureR ,TH2 * ExposureTOF,TH2 * ExposureNaF,TH2 * ExposureAgl );
+      void Set_Exposure_Time (TH2 * Exposure );
       void Set_Exposure_Time (TH1 * Tempi );
       TH1 * ExtractParticularMC_cs (TH1 * Histo, int lat_zones=1 ,int mc_type=0);
       void Set_DeltaE (int n,bool deutons=1);
-      void Add_SystFitError (int n, TH1* syst_errR,TH1* syst_errTOF,TH1* syst_errNaF,TH1* syst_errAgl);
+      void Add_SystFitError (int n, TH1* syst_err);
       void Eval_Flux (int n,bool deutons=1,int mc_type=0);
 };
 
@@ -74,9 +40,9 @@ class Flux {
 void Flux::Write()
 {
    if (Counts->GetEntries() >0) Counts->Write();
-
    return;
 }
+
 
 void Flux::Set_Exposure_Time (TH2 * hExposure )
 {
@@ -84,15 +50,20 @@ void Flux::Set_Exposure_Time (TH2 * hExposure )
    return;
 }
 
+
 void Flux::Set_Exposure_Time (TH1 * Tempi)
 {
-
    string hname=name + "TimeZone_" + suffixname;
    Exposure  = new TH2F ( hname.c_str(), hname.c_str(), nbinsr,  0,nbinsr,   Tempi->GetNbinsX(),0,Tempi->GetNbinsX() );
-   TimeGeo ( (TH2 *) Exposure  ,Tempi);
-
+      for (int R=0; R<Exposure ->GetNbinsX() ; R++ )
+      for (int lat =1; lat< Tempi->GetNbinsX(); lat++) {
+         (TH2 *) Exposure  -> SetBinContent (R+1,lat+1,Tempi -> GetBinContent (lat) );
+         (TH2 *) Exposure  -> SetBinError (R+1,lat+1,10);
+      }
    return;
 }
+
+
 
 TH1 * Flux::ExtractParticularMC_cs (TH1 * Histo, int lat_zones, int mc_type)
 {
@@ -123,79 +94,69 @@ void Flux::Set_DeltaE (int n,bool deutons)
       string hname=name + "DeltaE_" + suffixname;
       DeltaE   = new TH2F (  hname.c_str(), hname.c_str(), bins.size(),  0, bins.size(),  n,0,n);
       DeltaE   ->Sumw2();
-
-
-      if (deutons) {
-         for (int iR=0; iR<DeltaE_R->GetNbinsX(); iR++)
-            for (int lat =1; lat<DeltaE_R->GetNbinsX(); lat++) DeltaE_R->SetBinContent (iR+1,lat+1,deltaencindeut[iR]);
-      } else { //only in R bins p/d have different DeltaE
-         for (int iR=0; iR<DeltaE_R->GetNbinsX(); iR++)
-            for (int lat =1; lat<DeltaE_R->GetNbinsX(); lat++) DeltaE_R->SetBinContent (iR+1,lat+1,deltaencinprot[iR]);
-      }
-      for (int lat =0; lat<DeltaE->GetNbinsX(); lat++)
-         for (int iR=0; iR<DeltaE->GetNbinsX(); iR++)
-            DeltaE->SetBinContent (iR+1,lat+1, bins.EkBin(i) - bins.EkBin(i-1));
-
-
+      for (int iR=0; iR<DeltaE->GetNbinsX(); iR++)
+         for (int lat =0; lat<DeltaE->GetNbinsX(); lat++)
+            DeltaE->SetBinContent (iR+1,lat+1, bins.EkBin (i) - bins.EkBin (i-1) );
    } else { // 1D
-      DeltaE_R   = new TH1F ( (name + "DeltaE_R"   ).c_str(), (name + "DeltaE_R"   ).c_str(),nbinsr,  0,nbinsr  );
-
-
-      DeltaE_R   ->Sumw2();
-
-
-
-      for (int iR=1; iR<DeltaE_R->GetNbinsX(); iR++)  DeltaE_R->SetBinContent (iR+0,bins.EkBin(i) - bins.EkBin(i-1));
+      DeltaE   = new TH1F ( (name + "DeltaE_R"   ).c_str(), (name + "DeltaE_R"   ).c_str(),nbinsr,  0,nbinsr  );
+      DeltaE   ->Sumw2();
+      for (int iR=1; iR<DeltaE->GetNbinsX(); iR++)
+         DeltaE->SetBinContent (iR+0,bins.EkBin (i) - bins.EkBin (i-1) );
 
    }
 }
-void Flux::Add_SystFitError (int n, TH1* syst_errR,TH1* syst_errTOF,TH1* syst_errNaF,TH1* syst_errAgl)
+
+
+void Flux::Add_SystFitError (int n, TH1* syst_err)
 {
-   if (Counts_R  ) for (int iR = 0; iR < Counts_R  ->GetNbinsX(); iR++) Counts_R  -> SetBinError (iR+1,Counts_R  -> GetBinError (iR+1)+fabs (syst_errR->GetBinContent (iR+1) ) ) ;
-   if (Counts_TOF) for (int iR = 0; iR < Counts_TOF  ->GetNbinsX(); iR++) Counts_TOF  -> SetBinError (iR+1,Counts_TOF  -> GetBinError (iR+1)+fabs (syst_errTOF->GetBinContent (iR+1) ) ) ;
-   if (Counts_NaF) for (int iR = 0; iR < Counts_NaF  ->GetNbinsX(); iR++) Counts_NaF  -> SetBinError (iR+1,Counts_NaF  -> GetBinError (iR+1)+fabs (syst_errNaF->GetBinContent (iR+1) ) ) ;
-   if (Counts_Agl) for (int iR = 0; iR < Counts_Agl  ->GetNbinsX(); iR++) Counts_Agl  -> SetBinError (iR+1,Counts_Agl  -> GetBinError (iR+1)+fabs (syst_errAgl->GetBinContent (iR+1) ) ) ;
+   if (Counts)
+      for (int iR = 0; iR < Counts    ->GetNbinsX(); iR++)
+         Counts  ->   SetBinError (iR+1,Counts-> GetBinError (iR+1)+fabs (syst_err->GetBinContent (iR+1) ) ) ;
+   return;
+
 }
 
 
 void Flux::Eval_Flux (int n,bool deutons,int mc_type)
 {
-   Flux::Set_DeltaE (n,deutons);
+   Set_DeltaE (n,deutons);
 
-   if (Counts_R  ) 	Flux_R	= (TH1 *) Counts_R   -> Clone();
-   if (Counts_TOF)	Flux_TOF= (TH1 *) Counts_TOF -> Clone();
-   if (Counts_NaF)	Flux_NaF= (TH1 *) Counts_NaF -> Clone();
-   if (Counts_Agl)	Flux_Agl= (TH1 *) Counts_Agl -> Clone();
-
-   if (deutons) {
-      if (Counts_R  ) 	Flux_R	->Divide (ExtractParticularMC_cs ( Acceptance_R      ,n,mc_type) );
-      if (Counts_TOF)	Flux_TOF->Divide (ExtractParticularMC_cs ( Acceptance_TOF    ,n,mc_type) );
-      if (Counts_NaF)	Flux_NaF->Divide (ExtractParticularMC_cs ( Acceptance_NaF    ,n,mc_type) );
-      if (Counts_Agl)	Flux_Agl->Divide (ExtractParticularMC_cs ( Acceptance_Agl    ,n,mc_type) );
+   if (Counts) {
+      Fluxes = (TH1 *) Counts -> Clone();
+      if (deutons)  Fluxes->Divide (ExtractParticularMC_cs ( Acceptance , n,mc_type) );
+      else          Fluxes->Divide (Acceptance);
+      Fluxes->Divide (Exposure);
+      Fluxes->Divide (DeltaE);
    }
-
-   else {
-      if (Counts_R  ) 	Flux_R	->Divide (Acceptance_R  	);
-      if (Counts_TOF)	Flux_TOF->Divide (Acceptance_TOF	);
-      if (Counts_NaF)	Flux_NaF->Divide (Acceptance_NaF	);
-      if (Counts_Agl)	Flux_Agl->Divide (Acceptance_Agl	);
-   }
-
-   if (Counts_R  )	Flux_R	->Divide (Exposure_R 		);
-   if (Counts_TOF)  Flux_TOF->Divide (Exposure_TOF          );
-   if (Counts_NaF)  Flux_NaF->Divide (Exposure_NaF          );
-   if (Counts_Agl)  Flux_Agl->Divide (Exposure_Agl          );
-
-   if (Counts_R  )	Flux_R	->Divide (DeltaE_R 		);
-   if (Counts_TOF)  Flux_TOF->Divide (DeltaE_TOF            );
-   if (Counts_NaF)  Flux_NaF->Divide (DeltaE_NaF            );
-   if (Counts_Agl)  Flux_Agl->Divide (DeltaE_Agl            );
 
    return;
 }
 
 
+Flux::Flux (std::string basename, std::string suffix, Binning B)
+{
+   string hname=basename + "Counts_" + suffix;
+   Counts	 = new TH1F ( hname.c_str(), hname.c_str(),B.size(),0,B.size() );
+   name = basename;
+   suffixname = suffix;
+   bins=Bin;
+}
 
+Flux::Flux (std::string basename, std::string suffix, Binning B, int n)
+{
+   string hname=basename + "Counts_" + suffix;
+   Counts   = new TH2F ( hname.c_str(), hname.c_str(), B.size(),  0, B.size(),  n,0,n);
+   name = basename;
+   suffixname = suffix;
+   bins=Bin;
+}
 
-
-
+//reading constructor
+Flux::Flux (TFile * file, std::string basename, std::string suffix, std::string dirname, std::string acceptname, Binning B)
+{
+   Counts	 = (TH1 *) file->Get ( (basename + "Counts_" + suffix       ).c_str() );
+   Acceptance  = (TH1 *) file->Get ( ("/" + dirname +"/" +acceptname + "_" + suffix  ).c_str() );
+   name = basename;
+   suffixname = suffix;
+   bins=Bin;
+}
