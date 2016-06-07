da690480974248eca6ed3fbaad27f9713306b695
Laurent Basara <lbasara@lxplus.cern.ch>
2016-06-07 16:25:39 +0200

    (Broken) added Binning class, suppressed subclasses

--- Ntuple-making/include/GCRtest.cpp
+++ Ntuple-making/include/GCRtest.cpp
@@ -1,6 +1,10 @@
 #include "GCR_data.h"
+#include "Reweighting.cpp"
 
 int main() {
+   Particle proton(0.9382720813); // proton mass 938 MeV
+   Particle deuton(0.18756129);   // deuterium mass 1876 MeV
+   Binning bins(proton);
    GCR_data galprop;
    galprop.read("galprop.dat", "m2", "GeV");
    return 0;
