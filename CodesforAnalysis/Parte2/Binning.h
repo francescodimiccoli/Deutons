739c5525bbe8eae28fa6c36d82505f75319a13e1
Laurent Basara <lbasara@lxplus.cern.ch>
2016-06-07 09:15:01 +0200

    MC weighting, bug correction : adding to SetMatrix the case where MC bin is fully included in our bin

--- CodesforAnalysis/Parte2/Binning.h
+++ CodesforAnalysis/Parte2/Binning.h
@@ -285,7 +285,8 @@ void Binning::SetMatrix(std::vector<float> vinput)
          float tmin=rigbin[it], tmax=rigbin[it+1];
          float weight=0;
          // Which fraction of the bbin is in tbin? 4 possibilities:
-         if      (tmin > bmax || tmax < bmin ) weight=0;                      // either tbin is outside bbin
+         if      (tmin > bmax || tmax < bmin ) weight = 0;                    // either tbin is outside bbin
+         else if (tmin < bmin && tmax > bmax ) weight = 1;                    // or includes fully bbin
          else if (tmin > bmin && tmax < bmax ) weight = (tmax-tmin) / bwidth; // or fully included into bbin
          else if (tmin <=bmin && tmax < bmax ) weight = (tmax-bmin) / bwidth; // or partly included to the right
          else if (tmin > bmin && tmax >=bmax ) weight = (bmax-tmin) / bwidth; // ...or to the left
