5d96a4ec1ad5bb4a5b3cfdfc731f60abe49fdc9d
KKostya <kkanishev@gmail.com>
2016-12-09 16:02:26 +0100

    Fixing compilation on lxplus

---
+++ DSTcern/amsvar.lxplus.sh
@@ -0,0 +1,6 @@
+export AMSWD=/cvmfs/ams.cern.ch/Offline/vdev
+export AMSDataDir=/cvmfs/ams.cern.ch/Offline/AMSDataDir
+source /cvmfs/ams.cern.ch/Offline/root/Linux/root-v5-34-9-gcc64-slc6/bin/thisroot.sh
+export AMSLIBso=/cvmfs/ams.cern.ch/Offline/vdev/lib/linuxx8664gcc5.34/ntuple_slc6_PG.so
+source /afs/cern.ch/sw/lcg/external/gcc/4.9.3/x86_64-slc6/setup.sh
+
