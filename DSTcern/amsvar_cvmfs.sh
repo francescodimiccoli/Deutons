976ef922d0c687596093fbdba647f7eb9ee3e5eb
Konstantin Kanishchev <kostka@ui02-ams.cr.cnaf.infn.it>
2016-12-09 14:52:10 +0100

    DST code first commit

--- DSTcern/amsvar_cvmfs.sh
+++ DSTcern/amsvar_cvmfs.sh
@@ -3,3 +3,4 @@ export AMSDataDir=/cvmfs/ams.cern.ch/Offline/AMSDataDir
 source /cvmfs/ams.cern.ch/Offline/root/Linux/root-v5-34-9-gcc64-slc6/bin/thisroot.sh
 export AMSLIBso=/cvmfs/ams.cern.ch/Offline/vdev/lib/linuxx8664gcc5.34/ntuple_slc6_PG.so
 export LD_LIBRARY_PATH=/opt/exp_software/ams/additional_libs/lib64:$LD_LIBRARY_PATH
+source /afs/cern.ch/sw/lcg/external/gcc/4.9.3/x86_64-slc6/setup.sh
