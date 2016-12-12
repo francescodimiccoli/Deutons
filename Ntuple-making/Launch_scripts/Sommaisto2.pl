b5fd50bcf9094cfdd53ff003c8f73f79226bd538
Francesco <fra.dimiccoli@gmail.com>
2016-12-12 13:20:47 +0100

    grouping and changing names of launch scripts for analysis

--- Ntuple-making/Launch_scripts/Sommaisto2.pl
+++ Ntuple-making/Launch_scripts/Sommaisto2.pl
@@ -2,15 +2,13 @@
 
 
 #use warnings;
-system("rm /storage/gpfs_ams/ams/users/fdimicco/MAIN/Sommaisto*");
+system("rm /storage/gpfs_ams/ams/users/fdimicco/MAIN/SumScripts/Sommaisto*");
 
 print "Listing All Files..\n";
-chomp (@Rootuple = `ls  /storage/gpfs_ams/ams/users/fdimicco/MAIN/istogrammidati | grep -v "log" |  sed s/.root//g`);
-chomp (@Control =  `ls -la /storage/gpfs_ams/ams/users/fdimicco/MAIN/istogrammidati |grep "root" |grep -v "log" | awk '{print \$5}'`);
+chomp (@Rootuple = `ls  /storage/gpfs_ams/ams/users/fdimicco/MAIN/istogrammidatiCNAF | grep -v "log" |  sed s/.root//g`);
 $num_Rootuple = scalar(@Rootuple);
-$num_Contr = scalar(@Control);
 
-print "File Totali: ".$num_Rootuple." ".$num_Contr."\n";
+print "Total Files: ".$num_Rootuple."\n";
 @rootuple;
 $i=0;
 
@@ -22,34 +20,30 @@ for($n=0;$n<$num_Rootuple;$n++){
 }
 
 $num_rootuple = scalar(@rootuple);
-$controllo;
-print "File nel periodo richiesto: ".$num_rootuple."\n";
+print "Files in the requested period: ".$num_rootuple."\n";
 
 for ($n=0;$n<100; $n++)
 {
 
-	open(OUT,">","/storage/gpfs_ams/ams/users/fdimicco/MAIN/Sommaisto$n.sh");
+	open(OUT,">","/storage/gpfs_ams/ams/users/fdimicco/MAIN/SumScripts/Sommaisto$n.sh");
 	print OUT  "#!/bin/bash
 
 		hadd -f /storage/gpfs_ams/ams/users/fdimicco/MAIN/sommadati/sommadati$n.root ";
 
 	for ($j=($num_rootuple)/100*$n ; $j<($num_rootuple)/100*($n+1) ; $j++)
 	{
-		if(($rootuple[$j]!=$controllo)){
-				print OUT  " /storage/gpfs_ams/ams/users/fdimicco/MAIN/istogrammidati/$rootuple[$j].root "
-		}
-		$controllo=$rootuple[$j];
+				print OUT  " /storage/gpfs_ams/ams/users/fdimicco/MAIN/istogrammidatiCNAF/$rootuple[$j].root "
 	}
 	print OUT "\n";
 }
 
-open(OUT,">","/storage/gpfs_ams/ams/users/fdimicco/MAIN/Sommaisto.sh");
+open(OUT,">","/storage/gpfs_ams/ams/users/fdimicco/MAIN/SumScripts/Sommaisto.sh");
 
 print OUT  "#!/bin/bash";
 print OUT "\n";
 for ($n=0;$n<100; $n++)
 {
-	print OUT  "sh /storage/gpfs_ams/ams/users/fdimicco/MAIN//Sommaisto$n.sh\n";
+	print OUT  "sh /storage/gpfs_ams/ams/users/fdimicco/MAIN/SumScripts/Sommaisto$n.sh\n";
 }
 
 
--- perl/Create_lsfscripts.pl
+++ perl/Create_lsfscripts.pl
@@ -14,7 +14,7 @@ print OUT "#!/bin/bash
 
 export WORKDIR=$workdir
 source \$WORKDIR/amsvar.sh
-sh \$WORKDIR/../MAIN/Sommaisto$j.sh;
+sh \$WORKDIR/../MAIN/SumScripts/Sommaisto$j.sh;
 \$WORKDIR/Ntuple-making/AnalyzeDATA.exe $j $ARGV[0]>> \$WORKDIR/logs/log$j.log;\n
 \$WORKDIR/Ntuple-making/AnalyzeMC.exe $j $ARGV[0]>> \$WORKDIR/logs/logMC$j.log;\n
 \$WORKDIR/CodesforAnalysis/Analysis $ARGV[0] 0 $j \$WORKDIR >>\$WORKDIR/logs/logAnalysis$j.log;\n";
